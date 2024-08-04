b47be3a0aa0e17c27d13cd25717ab4911362cbe2
Rayyan Nafees <59685062+RayyanNafees@users.noreply.github.com>
2024-08-04 22:27:09 +0530

    Create dl.js

---
+++ dl.js
@@ -0,0 +1,12 @@
+// Download utilities for js
+
+function json(obj, name='file') {
+  const encoded = encodeURIComponent(JSON.stringify(storageObj))
+  const a = document.createElement('a')
+  a.href = 'data:text/json;charset=utf-8,' + encoded
+
+  if (!name.endsWith('.json'))
+      name += '.json';
+  a.download = name
+  a.click()
+}
